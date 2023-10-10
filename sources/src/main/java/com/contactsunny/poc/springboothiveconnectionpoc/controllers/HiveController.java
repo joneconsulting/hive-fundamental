d06da549863aba7e59ba7571d3f0fb09756d6a94
edowon0623@gmail.com <Dwlee$09>
2023-10-10 16:00:56 +0900

    added a sample code

---
+++ sources/src/main/java/com/contactsunny/poc/springboothiveconnectionpoc/controllers/HiveController.java
@@ -0,0 +1,45 @@
+package com.contactsunny.poc.springboothiveconnectionpoc.controllers;
+
+import com.contactsunny.poc.springboothiveconnectionpoc.services.HiveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hive")
+public class HiveController {
+
+	private final HiveService hiveService;
+
+	public HiveController(HiveService hiveService) {
+		this.hiveService = hiveService;
+	}
+
+	@RequestMapping(value = "/{schema}/tables", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Map<String, Object>>> getTablesForSchema(@PathVariable String schema) {
+		List<Map<String, Object>> rows = hiveService.getTables(schema);
+		return new ResponseEntity<>(rows, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/schemas", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Map<String, Object>>> getSchemas() {
+		List<Map<String, Object>> rows = hiveService.getSchemas();
+		return new ResponseEntity<>(rows, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{schema}/preview/{table}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Map<String, Object>>> previewTable(
+			@PathVariable String schema, @PathVariable String table
+	) {
+		List<Map<String, Object>> rows = hiveService.getTablePreview(schema, table);
+		return new ResponseEntity<>(rows, HttpStatus.OK);
+	}
+}
---
+++ sources/src/main/java/com/contactsunny/poc/springboothiveconnectionpoc/services/HiveService.java
@@ -0,0 +1,30 @@
+package com.contactsunny.poc.springboothiveconnectionpoc.services;
+
+import com.contactsunny.poc.springboothiveconnectionpoc.repositories.HiveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class HiveService {
+
+    private final HiveRepository hiveRepository;
+
+    public HiveService(HiveRepository hiveRepository) {
+        this.hiveRepository = hiveRepository;
+    }
+
+    public List<Map<String, Object>> getTables(String schema) {
+        return hiveRepository.getTables(schema);
+    }
+
+    public List<Map<String, Object>> getSchemas() {
+        return hiveRepository.getSchemas();
+    }
+
+    public List<Map<String, Object>> getTablePreview(String schema, String table) {
+        return hiveRepository.getTablePreview(schema, table);
+    }
+}
